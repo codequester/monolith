ce984a97bb8404d8bb23d5d5a6b514a861eb4d5c
Shankar Govindarajan <shgovind@shgovind-thinkpadp1gen4i.rmtustx.csb>
2024-07-10 23:45:15 -0500

    Changed the sql

--- monolith/tm-monolith/src/main/resources/import.sql
+++ monolith/tm-monolith/src/main/resources/import.sql
@@ -1,51 +1,51 @@
 -- MediaItem 0
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/65660684/640px-Weir%2C_Bob_(2007)_2.jpg')
 -- MediaItem 1
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/65660684/640px-Carnival_Puppets.jpg')
 -- MediaItem 2
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/65660684/640px-Opera_House_with_Sydney.jpg')
 -- MediaItem 3
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/65660684/640px-Roy_Thomson_Hall_Toronto.jpg')
 -- MediaItem 4
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/65660684/640px-West-stand-bmo-field.jpg')
 -- MediaItem 5
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/65660684/640px-Brazil_national_football_team_training_at_Dobsonville_Stadium_2010-06-03_13.jpg')
 -- MediaItem 6
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/AllStateFootballChampionship.png')
 -- MediaItem 7
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/ARhythmia.png')
 -- MediaItem 8
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/BattleoftheBrassBands.png')
 -- MediaItem 9
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/CarnivalComestoTown.png')
 -- MediaItem 10
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/ChrisLewisQuarterfinals.png')
 -- MediaItem 11
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/CrewYou.png')
 -- MediaItem 12
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/ExtremeSnowboardingFinals.png')
 -- MediaItem 13
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/FlamencoFinale.png')
 -- MediaItem 14
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/JesseLewisUnplugged.png')
 -- MediaItem 15
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/MadameButterfly.png')
 -- MediaItem 16
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/MimeMania.png')
 -- MediaItem 17
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/MorrisonCover.png')
 -- MediaItem 18
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/TutuTchai.png')
 -- MediaItem 19
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/SlapShot.png')
 -- MediaItem 20
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/Giantsofthegame.png')
 -- MediaItem 21
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://dl.dropbox.com/u/8625587/ticketmonster/Punch%26Judy.png')
 -- MediaItem 22
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Paris_Opera_full_frontal_architecture%2C_May_2009.jpg/800px-Paris_Opera_full_frontal_architecture%2C_May_2009.jpg')
 -- MediaItem 23
-insert into MediaItem ( mediaType, url) values ( 'IMAGE', '');
+insert into MediaItem ( mediaType, url) values ( 'IMAGE', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Boston_Symphony_Hall_from_the_south.jpg/800px-Boston_Symphony_Hall_from_the_south.jpg')
 
 -- Venue 1
 insert into Venue ( name, city, country, street, description, mediaitem_id, capacity) values ( 'Roy Thomson Hall', 'Toronto', 'Canada', '60 Simcoe Street','Roy Thomson Hall is the home of the Toronto Symphony Orchestra and the Toronto Mendelssohn Choir.',4, 11000);
@@ -140,7 +140,7 @@ insert into Event ( name, description, mediaitem_id, category_id) values ( 'Braz
 -- Event 4
 insert into Event ( name, description, mediaitem_id, category_id) values ( 'All State Football Championship', 'Show your colors in Friday Night Lights! Come see the Red Hot Scorpions put the sting on the winners of Sunday''s Coastal Quarterfinals for all state bragging rights. Fans entering the stadium in team color face paint will receive a $5 voucher redeemable with any on-site vendor. Body paint in lieu of clothing will not be permitted for this family friendly event.', 7, 4);
 -- Event 5
-insert into Event ( name, description, mediaitem_id, category_id) values ( 'Tennis Quarterfinals', 'For Every tennis enthusiast. Finals are already sold out, so do not miss your chance to see the real action in play on the eve of the big day!', 11, 4);
+insert into Event ( name, description, mediaitem_id, category_id) values ( 'Chris Lewis Quarterfinals', 'Every tennis enthusiast will want to see Wimbledon legend Chris Lewis as he meets archrival Deuce Wild in the quarterfinals of one of the top U.S. tournaments. Finals are already sold out, so do not miss your chance to see the real action in play on the eve of the big day!', 11, 4);
 -- Event 6
 insert into Event ( name, description, mediaitem_id, category_id) values ( 'Crew You', 'Join your fellow crew junkies and snarky, self-important collegiate know-it-alls from the nations snobbiest schools to see which team is in fact the fastest show on oars. (Or, if you like, just purchase a ticket and sport a t-shirt from your local community college just to tick them off -- this event promises to be SO much fun!)', 12, 4);
 -- Event 7
